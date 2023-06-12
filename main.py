ff0fe28c01840e84f4c9f99208b3eb0611238b26
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2023-06-12 13:28:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ def extract_information(file_contents):
     txt = f"""
     Information about the uploaded PDF: 
 
-    Author: {information.author}
-    Creator: {information.creator}
-    Producer: {information.producer}
-    Subject: {information.subject}
-    Title: {information.title}
+    Author: {information.author}\n
+    Creator: {information.creator}\n
+    Producer: {information.producer}|\n
+    Subject: {information.subject}\n
+    Title: {information.title}\n
     Number of pages: {number_of_pages}
     """
 
